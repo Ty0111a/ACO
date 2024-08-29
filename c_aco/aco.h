8e6429548867d719b127f73750a69bc0f44279eb
Ty0111a <wallposter24@gmail.com>
2024-08-29 07:22:58 +0000

    add another run

--- c_aco/aco.h
+++ c_aco/aco.h
@@ -15,7 +15,11 @@
 //__declspec(dllexport) void init_rand(time_t t);
 
 
-size_t* run(double** closeness_matrix, double** pheromone_matrix,
-    const size_t node_count, const size_t ant_count,
+size_t* run_fixed_generation(double** closeness_matrix, double** pheromone_matrix, 
+const size_t node_count, const size_t ant_count,
     const double A, const double B, const double Q, const double E, const size_t k, double* best_len);
  
+size_t* run_until_repeated_solution(double** closeness_matrix, double** pheromone_matrix, const size_t node_count, const size_t ant_count, const double A, const double B, const double Q, const double E, const size_t k, double* best_len); 
+
+size_t* run_until_stable_solution(double** closeness_matrix, double** pheromone_matrix, const size_t node_count, const size_t ant_count, const double A, const double B, const double Q, const double E, const size_t k, const double delta, double* best_len);
+
